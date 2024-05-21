c9691804d68eb55b77ef6d53136a6fd9e7feae5b
adeliiee22 <109847768+adeliiee22@users.noreply.github.com>
2024-05-21 20:49:17 +0700

    home.py

--- App/home.py
+++ App/home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-with open("style.css") as f:
+with open("App/style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 #####################
