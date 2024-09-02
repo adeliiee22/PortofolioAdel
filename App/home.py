e7b1cafbc78b19ab4720c97758fa1129fd3fdf30
adeliiee22 <109847768+adeliiee22@users.noreply.github.com>
2024-09-02 15:01:17 +0700

    home.py

--- App/home.py
+++ App/home.py
@@ -1,225 +1,170 @@
 import streamlit as st
-from PIL import Image
 
+# Load custom CSS
 with open("App/style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-#####################
-# Header 
-import streamlit as st
+# Sidebar for navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Home", "Education", "Work Experience", "Organization", "Projects", "Contact"])
 
-import streamlit as st
+if page == "Home":
+    # Home Page
+    st.write('''
+    # **[Dewi Adelia Priyono](https://www.linkedin.com/in/priyonoadelia/)** :fairy:
+    ''')
+    st.write('''
+    #### *Hi I'm Adel :wave:*
+    ''')  
+    st.write("A computer science student at UGM with experience in tech, reinsurance, and consulting. Skilled in data mining, big data processing, and data pipelining using Python, SQL, Hadoop, and Spark. Experienced with cloud computing and database tools like Oracle, Firebase, and AWS. Eager to expand my knowledge and skills before starting my professional career.")
+    
+elif page == "Education":
+    # Education Page
+    st.markdown('## Education')
 
-st.write('''
-# **[Dewi Adelia Priyono](https://www.linkedin.com/in/priyonoadelia/)** :fairy:
-''')
-st.write('''
-#### *Hi I'm Adel :wave:*
-''')  
-st.write("I am a computer science undergraduate student at UGM with experience in the tech, reinsurance, and consulting sectors. I have a strong foundation in data mining, big data processing, and data pipelining, with hands-on experience in Python and SQL, as well as working with Hadoop and Spark. Additionally, I have some experience with cloud computing and database integration tools such as Oracle, Firebase and AWS. I am continually seeking opportunities to broaden my horizons and enhance my skills before embarking on my professional career upon graduation.")
+    # Degree information
+    st.markdown("""
+    <div style="display: flex; justify-content: space-between;">
+        <span><strong>Bachelor Degree in Computer Science</strong>, UGM, Indonesia</span>
+        <span>2022-Present</span>
+    </div>
+    """, unsafe_allow_html=True)
 
-#####################
-# Navigation
+    # GPA
+    st.markdown("- GPA: `3.57`")
 
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+    # Create columns for Elective Courses
+    col1, col2 = st.columns(2)
 
-st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #5D4A69;">
-  <a class="navbar-brand" target="_blank">Dewi Adelia Priyono</a>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#education">Education</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#experience">Experience</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#documentation">Documentation</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#skills">Skills</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#projects">Projects</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#social-media">Social Media</a>
-      </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#certification">Certification</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-""", unsafe_allow_html=True)
+    with col1:
+        st.markdown("**Elective Courses:**")
+        st.markdown("""
+        - Genetic Algorithm  
+        - Big Data Analytics  
+        - Big Data Architecture  
+        """)
 
-#####################
-# Custom function for printing text
-def txt(a, b):
-  col1, col2 = st.columns([4,1])
-  with col1:
-    st.markdown(a)
-  with col2:
-    st.markdown(b)
+    with col2:
+        st.markdown("**  **")
+        st.markdown(""" 
+        - DataCenter Development  
+        - Digital Image Processing
+        """)
 
-def txt2(a, b):
-  col1, col2 = st.columns([1,4])
-  with col1:
-    st.markdown(f'`{a}`')
-  with col2:
-    st.markdown(b)
+    # Awards section
+    st.markdown("**Awards:**")
+    st.markdown("""
+    - Runner Up Internal Case Competition 180DC UGM 2023  
+    - 1st Place APWINC Digital Hackathon 2024  
+    - Best Participant Talent Scouting Academy Digital Talent Scholarship (TSA DTS) - Oracle Cloud 2024 by KOMINFO  
+    """)
 
-def txt3(a, b):
-  col1, col2 = st.columns([1,2])
-  with col1:
-    st.markdown(a)
-  with col2:
-    st.markdown(b)
-  
-def txt4(a, b, c):
-  col1, col2, col3 = st.columns([1.5,2,2])
-  with col1:
-    st.markdown(f'`{a}`')
-  with col2:
-    st.markdown(b)
-  with col3:
-    st.markdown(c)
+elif page == "Work Experience":
+    # Work Experience Page
+    st.markdown('## Work Experience')
 
-#####################
-st.markdown('''
-## Education
-''')
+    def txt(a, b):
+        col1, col2 = st.columns([4,1])
+        with col1:
+            st.markdown(a)
+        with col2:
+            st.markdown(b)
 
-txt('**Bachelor Degree in Computer Science**, *UGM*, Indonesia','*2022-Present*')
-st.markdown('''
-- GPA: `3.56`
-- Elective Courses:  `Genetic Algorithm, Big Data Analytics, Big Data Architecture, DataCenter Development, Digital Image Processing`
-- Competitions: `UGM Gemastik Data Mining Delegate 2023, UGM Satria Data Big Data Challenge Delegate 2023, DataQuest Universitas Airlangga 2023, DSW Telkomsel 2023`
-- Awards: `Talent Scouting Academy Digital Talent Scholarship (TSA DTS) - Oracle Cloud by KOMINFO`    
-''')
+    txt('**Data Engineer Intern**, Ucoal Resources', '*Jul 2024 - Sep 2024*')
+    st.markdown('''
+    - Built a robust data pipeline from MongoDB for 50k+ records, including extraction, cleaning, and preparation, to support comprehensive product and data analysis.
+    - Established data connections between a cloud database and Jupyter Notebook using PySpark, and implemented time series prediction models to analyze coal production trends.
+    - Created data transformation formulas in Excel for Power BI dashboards, and handled fraud detection in the hauling system for day-to-day monitoring of coal production.
+    ''')
 
+    txt('**Data Annotator**, KUPU ID', '*Apr 2024 - Jun 2024*')
+    st.markdown('''
+    - Led a comprehensive data labeling and verification project for ChatGPT-4, focusing on 500+ video and image datasets.
+    - Verified data quality, completeness, and reliability to enhance model accuracy and performance.
+    - Conducted thorough validation processes to improve data integrity and annotation consistency.
+    ''')
 
-#####################
-st.markdown('''
-## Experience
-''')
+elif page == "Organization":
+    # Organization Page
+    st.markdown('## Organization')
 
-txt('**Head, Big Data Division KOMASTAGAMA**, UGM',
-'*Present*')
-st.markdown('''
-- Spearheaded the design and implementation of a comprehensive division learning plan, fostering skill development and knowledge sharing among a diverse community of 60+ members. 
-- Led a dedicated internal team of 5 members in the routine evaluation of machine learning and data models, ensuring alignment with project objectives and quality standards.
-- Collaborated with esteemed lecturers to conduct cutting-edge research on emerging trends in data processing tools and methodologies.
-''')
+    def txt(a, b):
+        col1, col2 = st.columns([4,1])
+        with col1:
+            st.markdown(a)
+        with col2:
+            st.markdown(b)
 
-txt('**Research Leader,  180 Degrees Consulting UGM**, UGM',
-'*2023-2024*')
-st.markdown('''
-- Led cross-functional teams of 7 professionals to optimize product development processes through comprehensive data analysis.
-- Evaluated over 5 business cases and collaborated on 2 industry projects with NGOs and startup companies.
-- Delivered data-driven strategic blueprints and impact analyses, providing stakeholders with valuable insights to guide strategic decision-making processes.
-''')
+    txt('**Head, Big Data Division KOMASTAGAMA**, UGM', '*Jan 2024 - July 2024*')
+    st.markdown('''
+    - Spearheaded the design and implementation of a comprehensive division learning plan, fostering skill development and knowledge sharing among a diverse community of 60+ members. 
+    - Led a dedicated internal team of 5 members in the routine evaluation of machine learning and data models, ensuring alignment with project objectives and quality standards.
+    - Collaborated with esteemed lecturers to conduct cutting-edge research on emerging trends in data processing tools and methodologies.
+    ''')
 
-txt('**Staff, Data Mining and Artificial Intelligence**, KOMATIK, UGM',
-'2023-2024')
-st.markdown('''
-- Built and deployed predictive models and algorithms across diverse datasets, driving improved data
- accuracy and effectiveness
-- Conducted research for novel AI technologies, leveraging various datasets to fuel cutting-edge solutions.
-- Captained a research team called “Byte Benders” to 5+ Data and AI competitions on national and
- international level in 2023
-''')
+    txt('**Research Leader, 180 Degrees Consulting UGM**, UGM', '*Aug 2023 - Jun 2024*')
+    st.markdown('''
+    - Led cross-functional teams of 7 professionals to optimize product development processes through comprehensive data analysis.
+    - Evaluated over 5 business cases and collaborated on 2 industry projects with NGOs and startup companies.
+    - Delivered data-driven strategic blueprints and impact analyses, providing stakeholders with valuable insights to guide strategic decision-making processes.
+    ''')
 
-txt('**Regional Leader**, Generation Girl, Yogyakarta',
-'2022-2023')
-st.markdown('''
-- Led Generation Girl Jogja's operational team, ensuring smooth execution of programs and collaborating
-  with local partners like SkinGame and Innovative Academy UGM to deliver impactful STEM resources.
-- Designed and piloted Electives, a work-ready program in partnership with Gojek, empowering 240+
- STEM-focused girls annually through bootcamps, webinars, and competitions.
--  Championed STEM inclusion by launching impactful initiatives, attracting 20-30 girls monthly to
- Generation Girl Jogjakarta for the past 1 year.
-''')
+    txt('**Staff, Data Mining and Artificial Intelligence**, KOMATIK, UGM', '*Feb 2023 - Apr 2024*')
+    st.markdown('''
+    - Built and deployed predictive models and algorithms across diverse datasets, driving improved data accuracy and effectiveness.
+    - Conducted research for novel AI technologies, leveraging various datasets to fuel cutting-edge solutions.
+    - Captained a research team called “Byte Benders” to 5+ Data and AI competitions on national and international level in 2023.
+    ''')
 
-#####################
-st.markdown('''
-## Documentation
-''')
-# Creating three columns for the images and titles
-col1, col2, col3, col4= st.columns(4)
-col5, col6, col7, col8= st.columns(4)
+    txt('**Regional Leader**, Generation Girl, Yogyakarta', '*Jun 2022 - Jun 2023*')
+    st.markdown('''
+    - Led Generation Girl Jogja's operational team, ensuring smooth execution of programs and collaborating with local partners like SkinGame and Innovative Academy UGM to deliver impactful STEM resources.
+    - Designed and piloted Electives, a work-ready program in partnership with Gojek, empowering 240+ STEM-focused girls annually through bootcamps, webinars, and competitions.
+    - Championed STEM inclusion by launching impactful initiatives, attracting 20-30 girls monthly to Generation Girl Jogjakarta for the past 1 year.
+    ''')
 
-with col1:
-    st.image("App/dokum/1.jpg", use_column_width=True)
-with col2:
-    st.image("App/dokum/2.jpg", use_column_width=True)
-with col3:
-    st.image("App/dokum/3.jpg", use_column_width=True)
-with col4:
-    st.image("App/dokum/4.jpg", use_column_width=True)
-with col5:
-    st.image("App/dokum/5.jpg", use_column_width=True)
-with col6:
-    st.image("App/dokum/6.jpg", use_column_width=True)
-with col7:
-    st.image("App/dokum/7.jpg", use_column_width=True)
-with col8:
-    st.image("App/dokum/8.jpg", use_column_width=True)
+elif page == "Projects":
+    # Projects Page
+    st.markdown('## Projects')
 
+    # Creating columns for the project images and titles
+    col1, col2, col3 = st.columns(3)
+    col4, col5, col6 = st.columns(3)
 
-#####################
-st.markdown('''
-## Skills
-''')
-txt3('Programming', '`Python`, `C++`, `Java`')
-txt3('Data processing/wrangling', '`Sparks`,`SQL`, `pandas`, `numpy`, `Hadoop`')
-txt3('Data visualization', '`matplotlib`, `seaborn`, `plotly`, `ggplot2`')
-txt3('Machine Learning', '`scikit-learn`')
-txt3('Deep Learning', '`TensorFlow`')
-txt3('Web development', '`HTML`, `CSS`')
-txt3('Model deployment', '`streamlit`')
-txt3('Cloud environment', '`Oracle`, `AWS`, `Firebase`')
+    with col1:
+        st.image("App/image/LSTM.jpg", use_column_width=True)
+        st.markdown("[Stock Analysis](https://github.com/adeliiee22/Projects/tree/main/Stocks%20Analysis%20LSTM)", unsafe_allow_html=True)
 
-#####################
-st.markdown('''
-## Projects''')
+    with col2:
+        st.image("App/image/telecom.jpg", use_column_width=True)
+        st.markdown("[Churn](https://github.com/adeliiee22/Projects/tree/main/Telecom%20Churn)", unsafe_allow_html=True)
 
-# Creating three columns for the images and titles
-col1, col2, col3 = st.columns(3)
-col4, col5, col6 = st.columns(3)
+    with col3:
+        st.image("App/image/Chest.jpg", use_column_width=True)
+        st.markdown("[Chest Cancer Detection](https://github.com/adeliiee22/Projects/tree/main/Chest%20Cancer%20Detection/Notebook)", unsafe_allow_html=True)
 
-with col1:
-    st.image("App/image/LSTM.jpg", use_column_width=True)
-    st.markdown("[Stock Analysis](https://github.com/adeliiee22/Projects/tree/main/Stocks%20Analysis%20LSTM)", unsafe_allow_html=True)
+    with col4:
+        st.image("App/image/Perpus.jpg", use_column_width=True)
+        st.markdown("[Perpus UIN WebApp](http://158.178.244.40/)", unsafe_allow_html=True)
 
-with col2:
-    st.image("App/image/telecom.jpg", use_column_width=True)
-    st.markdown("[Churn](https://github.com/adeliiee22/Projects/tree/main/Telecom%20Churn)", unsafe_allow_html=True)
+    with col5:
+        st.image("App/image/Fraud.jpg", use_column_width=True)
+        st.markdown("[Credit Card Fraud Detection](https://github.com/adeliiee22/Projects/tree/main/Credit%20card%20fraud%20detection/Notebook)", unsafe_allow_html=True)
 
-with col3:
-    st.image("App/image/Chest.jpg", use_column_width=True)
-    st.markdown("[Chest Cancer Detection](https://github.com/adeliiee22/Projects/tree/main/Chest%20Cancer%20Detection/Notebook)", unsafe_allow_html=True)
+    with col6:
+        st.image("App/image/EHR.jpg", use_column_width=True)
+        st.markdown("[REKMED EHR Mobile App](https://github.com/adeliiee22/EHR-REKMED)", unsafe_allow_html=True)
 
-with col4:
-    st.image("App/image/Perpus.jpg", use_column_width=True)
-    st.markdown("[Perpus UIN WebApp](http://158.178.244.40/)", unsafe_allow_html=True)
+elif page == "Contact":
+    # Contact Page
+    st.markdown('## Contact')
 
-with col5:
-    st.image("App/image/Fraud.jpg", use_column_width=True)
-    st.markdown("[Credit Card Fraud Detection](https://github.com/adeliiee22/Projects/tree/main/Credit%20card%20fraud%20detection/Notebook)", unsafe_allow_html=True)
+    def txt2(a, b):
+        col1, col2 = st.columns([1,4])
+        with col1:
+            st.markdown(f'**{a}:**')
+        with col2:
+            st.markdown(b)
 
-with col6:
-    st.image("App/image/EHR.jpg", use_column_width=True)
-    st.markdown("[REKMED EHR Mobile App](https://github.com/adeliiee22/EHR-REKMED)", unsafe_allow_html=True)
-
-#####################
-st.markdown('''
-## Social Media
-''')
-txt2('LinkedIn', 'https://www.linkedin.com/in/priyonoadelia/')
-txt2('Email', 'priyonoadelia@gmail.com')
-
+    txt2('LinkedIn', 'https://www.linkedin.com/in/priyonoadelia/')
+    txt2('Email', 'priyonoadelia@gmail.com')
